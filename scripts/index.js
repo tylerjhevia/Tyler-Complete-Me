b21603a0b1fe59dc903d990b27462ba21dc258b6
Tyler Hevia <tylerhevia@gmail.com>
2017-07-19 14:27:40 -0600

    setting up webpack

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
-import Node from 'Node.js';
-import Trie from 'Trie.js';
+// import Node from 'Node.js';
+// import Trie from 'Trie.js';
 
-const wordInput = document.querySelector('.word-input');
-const selectButton = document.querySelector('.select-button');
+const wordInput = $('.word-input');
+const selectButton = $('.select-button');
+const wordList = $('.word-list');
 
 function check() {
   console.log('cool');
 }
 
-wordInput.addEventListener('input', check);
+function appendWord() {
+  let word = wordInput.val();
+  wordList.prepend(`<li>${word}</li>`);
+  clearInput();
+}
+
+function clearInput() {
+  wordInput.val('');
+}
+
+wordInput.on('input', check);
+selectButton.on('click', appendWord);
